d9b299421939bc3268a8d44fc1a27bfdd5d6c875
liqimai <liqimai@outlook.com>
2017-08-25 13:37:34 +0800

    make 526 cleaner

--- 526/526.go
+++ 526/526.go
@@ -27,6 +27,28 @@ func main() {
     fmt.Println(time.Since(start))
 }
 
+func countArrangement(N int) int {
+    visited := make([]bool, N + 1)
+    count, total := 0,0
+    calculate(N, 1, visited, &count, &total)
+    fmt.Printf("%v/%v=%v\n", count,total,float64(count)/float64(total))
+    return count
+}
+func calculate(N int, pos int, visited []bool, count *int, total *int) {
+    *total += 1
+    if pos > N {
+        *count++
+        return
+    }
+    for i := 1; i <= N; i++ {
+        if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+            visited[i] = true
+            calculate(N, pos + 1, visited, count, total)
+            visited[i] = false
+        }
+    }
+}
+
 // func swapWithRightMostSuccessor(nums []int, pivot int){
 //     //find rightmost successor
 //     var rs int
@@ -91,65 +113,65 @@ func main() {
 
 
 
-func countArrangement(N int) int {
-    var valid = make([][]bool, N)
-    for i := range valid {
-        valid[i] = make([]bool, N)
-        for j := range valid[i] {
-            valid[i][j] = (i + 1) % (j + 1) == 0 || (j + 1) % (i + 1) == 0
-        }
-        // fmt.Println(valid[i])
-    }
-    // p := (*reflect.SliceHeader)(unsafe.Pointer(&valid))
-    // fmt.Printf("p.len, p.cap, p.data = %v, %v, %v\n", p.Len, p.Cap, p.Data)
-
-    var nums = make([]int, N)
-    for i := range nums{
-        nums[i] = i + 1
-    }
-    var count = 0
-    var total = 0
-    for {
-        total++
-        // fmt.Println(nums)
-        var index = noRepeat(nums)
-        if index == len(nums){
-            count++
-            index--
-        }
-        if carry := addOne(nums, &valid, index); carry {
-            break
-        }
-    }
-    fmt.Printf("total=%v, count/total=%v\n", total, float64(count)/float64(total))
-    return count
-}
-func noRepeat(nums []int) int{
-    var flag = make([]bool, len(nums))
-    for i, v := range nums {
-        if flag[v-1] == true{
-            return i
-        }
-        flag[v-1] = true
-    }
-    return len(nums)
-}
-func addOne(nums []int, valid *[][]bool, i int) bool{
-    var carry = false
-    for j := nums[i]; true ;  {
-        if j >= len(nums) {
-            carry = true
-        }
-        j %= len(nums)
-        if (*valid)[i][j] {
-            nums[i] = j + 1
-            break
-        }
-        j ++
-    }
-    if carry && i>0{
-        return addOne(nums, valid, i-1)
-    }else{
-        return carry
-    }
-}
+// func countArrangement(N int) int {
+//     var valid = make([][]bool, N)
+//     for i := range valid {
+//         valid[i] = make([]bool, N)
+//         for j := range valid[i] {
+//             valid[i][j] = (i + 1) % (j + 1) == 0 || (j + 1) % (i + 1) == 0
+//         }
+//         // fmt.Println(valid[i])
+//     }
+//     // p := (*reflect.SliceHeader)(unsafe.Pointer(&valid))
+//     // fmt.Printf("p.len, p.cap, p.data = %v, %v, %v\n", p.Len, p.Cap, p.Data)
+//
+//     var nums = make([]int, N)
+//     for i := range nums{
+//         nums[i] = i + 1
+//     }
+//     var count = 0
+//     var total = 0
+//     for {
+//         total++
+//         // fmt.Println(nums)
+//         var index = noRepeat(nums)
+//         if index == len(nums){
+//             count++
+//             index--
+//         }
+//         if carry := addOne(nums, &valid, index); carry {
+//             break
+//         }
+//     }
+//     fmt.Printf("total=%v, count/total=%v\n", total, float64(count)/float64(total))
+//     return count
+// }
+// func noRepeat(nums []int) int{
+//     var flag = make([]bool, len(nums))
+//     for i, v := range nums {
+//         if flag[v-1] == true{
+//             return i
+//         }
+//         flag[v-1] = true
+//     }
+//     return len(nums)
+// }
+// func addOne(nums []int, valid *[][]bool, i int) bool{
+//     var carry = false
+//     for j := nums[i]; true ;  {
+//         if j >= len(nums) {
+//             carry = true
+//         }
+//         j %= len(nums)
+//         if (*valid)[i][j] {
+//             nums[i] = j + 1
+//             break
+//         }
+//         j ++
+//     }
+//     if carry && i>0{
+//         return addOne(nums, valid, i-1)
+//     }else{
+//         return carry
+//     }
+// }
